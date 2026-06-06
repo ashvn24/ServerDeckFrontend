ead869559c67bbd672228cde65d1d18200fca3b8
ashwin@blackrock <ashwin@blackrockitsolutions.com>
2026-06-06 14:35:52 +0530

    url mismatch

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://api.serverdeck.online';
+export const API_BASE_URL = 'https://api.serverdeck.online/api';
 export const WS_URL = 'wss://api.serverdeck.online/ws/client';
